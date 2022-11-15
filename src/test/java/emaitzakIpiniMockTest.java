a4b5df1881fbb8bc27ced54682ff7c62c449efa3
paabloomaartinez <pablo121002@gmail.com>
2022-11-15 23:18:21 +0100

    ok

--- src/test/java/emaitzakIpiniMockTest.java
+++ src/test/java/emaitzakIpiniMockTest.java
@@ -1,5 +1,4 @@
-
-import businessLogic.BLFacadeImplementation;
+import businessLogic.BLFacade;
 import configuration.UtilDate;
 import dataAccess.DataAccess;
 import domain.ApustuAnitza;
@@ -9,66 +8,77 @@ import domain.Question;
 import domain.Quote;
 import domain.Registered;
 import domain.Team;
-import exceptions.EventNotFinished;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+
 
 
 public class emaitzakIpiniMockTest {
 	
-	DataAccess mock = Mockito.mock(DataAccess.class);
-	BLFacadeImplementation sut = new BLFacadeImplementation(mock);
-	
-	Team team1= new Team("Atletico");
-	Team team2= new Team("Athletic");
-	Event ev1=new Event(1, "Atletico-Athletic", UtilDate.newDate(2022,1,17), team1, team2);
-	Question q1 = new Question("¿Quién ganará el partido?",1, ev1);
-	Quote quote1 = q1.addQuote(1.3, "1", q1); 
+	@Mock
+	DataAccess mock;
+	@InjectMocks
+	BLFacade sut;
 
+	 /* Salta excepcion por fecha del evento todavia no finalizada **/
 	@Test
 	public void test1() {
 		
-		Registered reg1 =new Registered("registered", "123", 1234);
-		ApustuAnitza apA1 = new ApustuAnitza(reg1, 5.0);
-		Apustua ap1 = new Apustua(apA1, quote1);
-		apA1.addApustua(ap1);
-	
-		ArgumentCaptor<Quote> cuota1 = ArgumentCaptor.forClass(Quote.class);
-		
 		try {
-			Mockito.verify(mock, Mockito.times(1)).EmaitzakIpini(cuota1.capture());
-		} catch (EventNotFinished e) {
+			
+			Team team1= new Team("Atletico");
+			Team team2= new Team("Athletic");
+			Event ev1=new Event(1, "Atletico-Athletic", UtilDate.newDate(2023,1,17), team1, team2);
+			Question q1 = new Question("¿Quién ganará el partido?",1, ev1);
+			Quote quote1 = q1.addQuote(1.3, "1", q1); 	
+			Registered reg1 =new Registered("registered", "123", 1234);
+			ApustuAnitza apA1 = new ApustuAnitza(reg1, 5.0);
+			Apustua ap1 = new Apustua(apA1, quote1);
+			apA1.addApustua(ap1);
+			
+			try {
+				
+				sut.EmaitzakIpini(quote1);
+			}catch(Exception EventNotFinished) {
+				System.out.println("EventNotFinished");
+				assertTrue(true);
 
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		assertEquals(quote1, cuota1.getValue());
 		
 	}
 	
-	
 	@Test
-    public void test2() {
+	public void test2() {
+		ArgumentCaptor<Quote> cuota1 = ArgumentCaptor.forClass(Quote.class);
+		
+		try {
+					
+			Team team1= new Team("Atletico");
+			Team team2= new Team("Athletic");
+			Event ev1=new Event(1, "Atletico-Athletic", UtilDate.newDate(2021,1,17), team1, team2);
+			Question q1 = ev1.addQuestion("¿Quién ganará el partido?",1);
+			Quote quote1 = q1.addQuote(1.3, "1", q1);
+			
+			try {
+				
+				sut.EmaitzakIpini(quote1);
+				Mockito.verify(mock, Mockito.times(1)).EmaitzakIpini(cuota1.capture());
+				assertEquals(quote1, cuota1.getValue());
 
-            try {
+			}catch(Exception EventNotFinished) {
+				System.out.println("EventNotFinished");
 
-                Mockito.doReturn(new EventNotFinished()).when(mock).EmaitzakIpini(quote1);
-                sut.EmaitzakIpini(quote1);
-
-            } catch (EventNotFinished e1) {
-                assertTrue(true);
-                System.out.println("EventNotFinished");
-
-            }
-
-            ArgumentCaptor<Quote> cuota2 = ArgumentCaptor.forClass(Quote.class);
-            try {
-                Mockito.verify(mock, Mockito.times(1)).EmaitzakIpini(cuota2.capture());
-            } catch (EventNotFinished e) {
-                fail("No es posible");
-            }
-            assertEquals(quote1, cuota2.getValue());
-    }
-	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
