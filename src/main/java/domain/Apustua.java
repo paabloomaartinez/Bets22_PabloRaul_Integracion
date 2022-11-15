db8fc015602d795a526d94f15ed23e1fd80ae843
paabloomaartinez <pablo121002@gmail.com>
2022-11-15 21:34:55 +0100

    paso final

--- src/main/java/domain/Apustua.java
+++ src/main/java/domain/Apustua.java
@@ -4,10 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
